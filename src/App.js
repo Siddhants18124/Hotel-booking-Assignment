40d1240b452f875fd5d9aaa246ae208608d4c487
siddhant <siddhant.sharma.21cse@bmu.edu.in>
2024-12-26 21:55:02 +0530

    Fixing build error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
-import Hero from "./components/Hero/Hero";
 import HotelDetails from "./components/HotelDetails/HotelDetails";
 import HotelList from "./components/HotelList/HotelList";
-import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
